27bb4eb7e8c27e26e821e9f678a8fc1fffded595
goyalsgit <goyalsgit@gmail.com>
2025-10-03 20:08:30 +0530

    Your update message

--- light/src/App.jsx
+++ light/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 '>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-yellow-50 px-3 py-2 rounded-3xl'>
             <button className=' h-12 outline-none px-4 rounded-full text-white ease-in-out hover:scale-110'onClick={()=>{setColor("red")}} style={{backgroundColor:"red"}}>Red</button>
-            <button className='outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("orange")}}style={{backgroundColor:"orange"}}>Orange</button>
-            <button className='outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("green")}}style={{backgroundColor:"green"}}>green</button>            
-            <button className='outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("blue")}} style={{backgroundColor:"blue"}}>blue</button>
-            <button className='outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("purple")}} style={{backgroundColor:"purple"}}>purple</button>
-            <button className='outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("brown")}}style={{backgroundColor:"brown"}}>brown</button>
+            <button className=' h-12 outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("orange")}}style={{backgroundColor:"orange"}}>Orange</button>
+            <button className='h-12 outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("green")}}style={{backgroundColor:"green"}}>green</button>            
+            <button className=' h-12 outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("blue")}} style={{backgroundColor:"blue"}}>blue</button>
+            <button className='h-12 outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("purple")}} style={{backgroundColor:"purple"}}>purple</button>
+            <button className='h-12 outline-none px-4 rounded-full text-white ease-in-out hover:scale-110' onClick={()=>{setColor("brown")}}style={{backgroundColor:"brown"}}>brown</button>
         </div>
       </div>
     </div>
